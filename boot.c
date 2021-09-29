27391d10dc115318aa05584d629ac634e9894eec
aramya <22577625+thamugadi@users.noreply.github.com>
2021-09-29 16:43:27 +0100

    memory map

--- boot.c
+++ boot.c
@@ -1,11 +1,28 @@
+#define DEVICE16               0x81080000
+#define DEVICE16END            0x810FFFFF
+#define VRAM                   0x80000000
+#define VRAMEND                0x80FFFFFF
+#define BIOS	               0xFFC00000
+#define BIOSEND                0xFFFFFFFF
 void begin(void)
 {
-	unsigned char* addr = 0x7FFFFFFF;
-	unsigned char* noise = 0xFFEE0000;
-	while (addr < 0x80500000){
-		*addr = *noise;
+	unsigned char* addr = VRAM;
+	unsigned char* noise = BIOS+0x2E0000;
+	while (addr < VRAMEND)
+	{
+		*addr = *noise; 
 		addr++;
 		noise++;
 	}
-	for(;;);
+	begin();
+}
+
+void clearscreen(void)
+{
+	unsigned char* addr = 0x80000000;
+	while (addr < 0x80FFFFFF)
+	{
+		*addr = 0;
+		addr++;
+	}
 }
