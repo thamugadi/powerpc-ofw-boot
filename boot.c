ebbb9af0b8f1e9866aa415c015cc4ede98597b98
thamugadi <qirimhangli@gmail.com>
2021-09-30 03:19:50 +0100

    added mac99 support

--- boot.c
+++ boot.c
@@ -1,28 +1,9 @@
-#define DEVICE16               0x81080000
-#define DEVICE16END            0x810FFFFF
-#define VRAM                   0x80000000
-#define VRAMEND                0x80FFFFFF
-#define BIOS	               0xFFC00000
-#define BIOSEND                0xFFFFFFFF
 void begin(void)
 {
-	unsigned char* addr = VRAM;
-	unsigned char* noise = BIOS+0x2E0000;
-	while (addr < VRAMEND)
-	{
-		*addr = *noise; 
-		addr++;
-		noise++;
-	}
-	begin();
-}
-
-void clearscreen(void)
-{
-	unsigned char* addr = 0x80000000;
-	while (addr < 0x80FFFFFF)
-	{
-		*addr = 0;
-		addr++;
-	}
+	unsigned char type;
+	unsigned char *ptr_beige = 0x80000000;
+	unsigned char *ptr_mac99 = 0x81000000;
+	if (*ptr_beige == 0xBE) type = 0xBE;
+	if (*ptr_mac99 == 0x5A) type = 0x5A;
+	for(;;);
 }
