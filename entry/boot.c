5793603952e28c5ccb53496b5199486af3a46df0
thamugadi <qirimhangli@gmail.com>
2021-10-10 20:44:34 +0100

    memcpy

--- entry/boot.c
+++ entry/boot.c
@@ -6,13 +6,12 @@
 
 #define __VRAM__MAC99           0x81000000
 #define __BIOS__MAC99           0XFFF00000
-#define __USB_KEYBOARD__MAC99   0x80080038
-#define __USB_MOUSE__MAC99      0x8008003C
 
 unsigned char  IO_TYPE;
 unsigned char* p_vram;
 unsigned char* p_bios;
-
+unsigned char* p_usb_keyboard;
+unsigned char* p_usb_mouse;
 void get_io_type(void)
 {
         if (*(unsigned char*)__VRAM__BEIGE == beige) IO_TYPE = beige;
@@ -36,11 +35,12 @@ void main(void)
 {
 	get_io_type();
 	init();
-	clearscreen(100,85,100, 0x100000);
-	for(;;);
+	//fillscreen(85,100,85, 0x100000);
+	for(;;) memcpy(0x81000000, 0, 100000);
+	
 }
 
-void clearscreen(unsigned char a, unsigned char b, unsigned char c, int n)
+void fillscreen(unsigned char a, unsigned char b, unsigned char c, int n)
 {
 	init();
 	for (unsigned int i = 0; i<n; i++)
@@ -54,5 +54,17 @@ void clearscreen(unsigned char a, unsigned char b, unsigned char c, int n)
 		p_vram++;
 		*p_vram = c;
 		p_vram++;
+	}
+}
+
+void memcpy(unsigned char* dest, unsigned char* src, int n)
+{
+	unsigned char* destination = dest;
+	unsigned char* source = src;
+	for (int i = 0; i<n; i++)
+	{
+		*destination = *source;
+		destination++;
+		source++;
 	}
 }
