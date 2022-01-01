82a2c992d994f69f3059324b0035accd258aeb88
aramya <22577625+thamugadi@users.noreply.github.com>
2022-01-01 03:46:13 +0100

    conway's game of life

--- entry/boot.h
+++ entry/boot.h
@@ -3,22 +3,25 @@
 
 #define __VRAM__BEIGE           0x80000000
 #define __BIOS__BEIGE           0xFFC00000
-
+#define __TIMR__BEIGE           0x1badc0de //later
 #define __VRAM__MAC99           0x81000000
-#define __BIOS__MAC99           0XFFF00000
-#define __MOUSE__MAC99          0x8008003C
-#define __KEYBOARD__MAC99       0x80080038
+#define __BIOS__MAC99           0xFFF00000
+#define __TIMR__MAC99           0x80080038
+void __stack_chk_fail_local();
+
 unsigned char  IO_TYPE;
+unsigned int   screen_width;
+unsigned int   screen_height;
+
 unsigned char* p_vram;
 unsigned char* p_bios;
-unsigned char* p_keyboard;
-unsigned char* p_mouse;
-
+unsigned char* p_timer;
 unsigned char u8_extract_bit(unsigned char n, unsigned char bit);
 void get_io_type(void);
 void init(void);
-void fillscreen(unsigned char a, unsigned char b, unsigned char c, int n);
+void fillscreen(unsigned int* addr, unsigned char a, unsigned char b, unsigned char c, int n);
 void memcpy(unsigned char* dest, unsigned char* src, int n);
-void memcpy_24bit(unsigned char* dest, unsigned char* src, int n, int size);
 unsigned char u8_extract_bit(unsigned char n, unsigned char bit);
 unsigned char u32_extract_bit(unsigned int n, unsigned char bit);
+void colorscreen(unsigned int* addr, unsigned char a, unsigned char b, unsigned char c, int n);
+
--- loader/load.fth
+++ loader/load.fth
@@ -4,21 +4,23 @@
 : sup - dup abs = ;
 : inf dup sup 1 + ; 
 : diff = if 0 else -1 then ;
-
 : fba frame-buffer-adr ;
 : beige-vram 80000000 ; : mac99-vram 81000000 ;
-
-: beige-error ." G3 Beige machine is not supported yet. Please run it with a post-1999 PowerPC Mac." ;
 : hardware-error ." Hardware not supported." ;
-variable run 
+
+fba beige-vram = if ." Beige hardware detected" cr 0BE beige-vram c! then 
+fba mac99-vram = if ." mac99 hardware detected" cr 05A mac99-vram c! then
+
+fba beige-vram = if screen-width  4 beige-vram + l! then
+fba beige-vram = if screen-height 8 beige-vram + l! then
+
+fba mac99-vram = if screen-width  4 mac99-vram + l! then
+fba mac99-vram = if screen-height 8 mac99-vram + l! then
+
+variable run
 -1 run !
-
-fba beige-vram = if beige-error cr 100 0 do 0BE i beige-vram + c! loop then 
-fba mac99-vram = if ." mac99"   cr 100 0 do 05A i mac99-vram + c! loop then
 fba beige-vram diff fba mac99-vram diff and if hardware-error cr 0 run ! then
-
 run @ 0 = if 1 0 do 0 +loop then
-80080000 100 dump
-80080000 100 dump
-80080000 100 dump
 boot hd:,\boot\kernel.elf
+
+
