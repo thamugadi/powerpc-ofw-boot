7639500d7f899eaae80239bd77bd7c3c32f1c68d
aramya <22577625+thamugadi@users.noreply.github.com>
2025-06-25 12:33:38 +0100

    header

--- include/ofw.h
+++ include/ofw.h
@@ -13,3 +13,12 @@ void puts(char* str, int len);
 
 void* ofw_interpret(char* cmd, int32_t* stack_args, int n_stack_args, int n_ret_args, int32_t* retaddr);
 int32_t ofw_test(char* name);
+
+ihandle ofw_open(char* device_specifier);
+void ofw_close(ihandle instance);
+int32_t ofw_read(ihandle instance, void* addr, int32_t len);
+int32_t ofw_write(ihandle instance, void* addr, int32_t len);
+int32_t ofw_seek(ihandle instance, int32_t pos_hi, int32_t pos_lo);
+
+void* ofw_set_callback(void* addr);
+void ofw_set_symbol_lookup(void* sym_to_value, void* value_to_sym);
